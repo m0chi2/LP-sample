2a6163b98ad1ee7d8e0d8af88f1034781f78043b
m0chi2 <20saki19@gmail.com>
2020-09-15 23:54:39 +0000

    [Add]feature page

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  const spinner = document.getElementById('loader-bg');
+  spinner.classList.add('loaded');
+}
+// var bg = document.getElementById('loader-bg'),
+//     loader = document.getElementById('loader');
+
+// bg.classList.remove('is-hidden');
+// loader.classList.remove('is-hidden');
+
+// window.addEventListener('load', stopload);
+
+// setTimeout('stopload()',10000);
+// function stopload(){
+//     bg.classList.add('fadeout-bg');
+//     loader.classList.add('fadeout-loader');
+// }
