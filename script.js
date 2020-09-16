8958c61392ed41418e50a75e478008f29112b86f
m0chi2 <20saki19@gmail.com>
2020-09-16 15:49:05 +0000

    [Add]feature page2

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-window.onload = function() {
-  const spinner = document.getElementById('loader-bg');
-  spinner.classList.add('loaded');
+var bg = document.getElementById('loader-bg');
+var loader = document.getElementById('loader');
+
+bg.classList.remove('hide');
+loader.classList.remove('hide');
+
+window.addEventListener('load', stopload);
+
+setTimeout('stopload()',10000);
+function stopload(){
+    bg.classList.add('fadeout-bg');
+    loader.classList.add('fadeout-loader');
 }
-// var bg = document.getElementById('loader-bg'),
-//     loader = document.getElementById('loader');
-
-// bg.classList.remove('is-hidden');
-// loader.classList.remove('is-hidden');
-
-// window.addEventListener('load', stopload);
-
-// setTimeout('stopload()',10000);
-// function stopload(){
-//     bg.classList.add('fadeout-bg');
-//     loader.classList.add('fadeout-loader');
-// }
