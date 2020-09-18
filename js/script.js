65d615069a3dd4b1ecb577cc5786b295f9ce3716
m0chi2 <20saki19@gmail.com>
2020-09-18 16:11:34 +0000

    [Add]move test

--- js/script.js
+++ js/script.js
@@ -16,4 +16,31 @@ function stopload(){
 	var loader = document.getElementById('loader');
     bg.classList.add('fadeout-bg');
     loader.classList.add('fadeout-loader');
-}
+};
+
+$(function() {
+	var h = $(window).height();
+	var speed = 200;
+	$('.contents').each(function(){
+		$(this).css({height:h}
+	    );
+    });
+
+	$(window).on('scroll',function(){
+		var value = $(this).scrollTop();
+		$('.fadeDone').each(function(){
+			var fadeTop = $(this).offset().top;
+			var fadeShow = fadeTop - h/1.1;
+			if(value >= fadeShow) {
+				$(this).stop().animate({
+					opacity: 1
+				}, speed);
+			}else{
+				$(this).stop().animate({
+					opacity: 0
+				}, speed);
+			}
+		});
+		$('#frame')
+	});
+});
